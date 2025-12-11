dc8caaeebebfa5395a1e54562805ab43b4aa6a97
Yellowscar <Yellowmushroom83006@gmail.com>
2025-12-10 20:10:28 -0600

    Added seperate tits
    Added assets for seperate tits. Not sure whether we should use the combined or separated sprites, might need to keep both and alternate between the two based on the situation.

--- milk-flood-v-1/Scripts/boob_manager.gd
+++ milk-flood-v-1/Scripts/boob_manager.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var ComboBoob = %"Combined Boob"
-
+@onready var LeftTit = %"Left Tit"
+@onready var RightTit = %"Right Tit"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	%"Combined Boob".play("StraightFromTap1")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
